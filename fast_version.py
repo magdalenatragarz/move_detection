468e7124fddf9e131399549153da4e2815db95ad
Magda <magdtrag@gmail.com>
2018-11-28 14:04:08 +0100

    person update version

--- fast_version.py
+++ fast_version.py
@@ -1,85 +1,143 @@
-# import the necessary packages
-from imutils.video import VideoStream
-import argparse
 import datetime
 import imutils
-import time
 import cv2
+import math
 
-video_path = 'D:\krk4.mp4'
+people = []
 
-vs = cv2.VideoCapture(video_path)
 
-# initialize the first frame in the video stream
-firstFrame = None
+class Person:
+    x = 0
+    y = 0
+    w = 0
+    h = 0
 
-# loop over the frames of the video
-while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    frame = vs.read()
-    frame = frame[1]
-    text = "Unoccupied"
+    current_state = (x,y)
+    history = []
 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
-        break
+    def __init__(self,x,y,w,h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.current_state = (x,y)
+        self.history.append((x,y,w,h))
 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    def update(self,x,y,w,h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.current_state = (x, y)
+        self.history.append((x, y, w, h))
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
+    def dist(self,x,y):
+        return math.hypot(x - self.x, y - self.y)
+# --------------------------------------
 
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+def main():
+    video_path = 'D:\krk.mp4'
 
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < 50 or cv2.contourArea(c) > 200:
+    vs = cv2.VideoCapture(video_path)
+    # initialize the first frame in the video stream
+    firstFrame = None
+    # loop over the frames of the video
+    while True:
+        # grab the current frame and initialize the occupied/unoccupied
+        # text
+        frame = vs.read()
+        frame = frame[1]
+        text = "Unoccupied"
+
+        # if the frame could not be grabbed, then we have reached the end
+        # of the video
+        if frame is None:
+            break
+
+        # resize the frame, convert it to grayscale, and blur it
+        #frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        # if the first frame is None, initialize it
+        if firstFrame is None:
+            firstFrame = gray
             continue
 
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
+        # compute the absolute difference between the current frame and
+        # first frame
+        frameDelta = cv2.absdiff(firstFrame, gray)
+        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-        if y < 200:
-            continue
+        # dilate the thresholded image to fill in holes, then find contours
+        # on thresholded image
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
+        # loop over the contours
+        for c in cnts:
+            # if the contour is too small, ignore it
+            if cv2.contourArea(c) < 100 or cv2.contourArea(c) > 300:
+                continue
 
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+            # compute the bounding box for the contour, draw it on the frame,
+            # and update the text
+            (x,y,w,h) = cv2.boundingRect(c)
 
-    # show the frame and record if the user presses a key
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
+            if y < 300:
+                continue
 
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
+            if not people:
+                people.append(Person(x, y, w, h))
+                continue
 
-# cleanup the camera and close any open windows
-vs.release()
-cv2.destroyAllWindows()
+            updated = False
+            print(len(people))
+            for person in people:
+                print("porownanie wspolrzednych:", x, ", ",y , " z ", person.x, ", ", person.y)
+                print("wynik:")
+                if person.dist(x,y) < 50:
+                    person.update(x,y,w,h)
+                    print("update")
+                    updated = True
+                    break
+            if updated==False:
+                people.append(Person(x, y, w, h))
+                print("dodaje nowa")
+
+
+            #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        for person in people:
+            cv2.rectangle(frame, (person.x, person.y), (person.x + person.w, person.y + person.h), (0, 255, 0), 2)
+
+        # draw the text and timestamp on the frame
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+                    (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+        # show the frame and record if the user presses a key
+        cv2.imshow("Security Feed", frame)
+        cv2.imshow("Thresh", thresh)
+        cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
+
+    # cleanup the camera and close any open windows
+    vs.release()
+    cv2.destroyAllWindows()
+
+
+#x = Person(1,2,3,4)
+
+#print(x.dist(2,5))
+
+main()
+
--- ok_detection.py
+++ ok_detection.py
@@ -1,13 +1,51 @@
 import cv2
+import math
+
+people = []
+
+
+class Person:
+    x = 0
+    y = 0
+    w = 0
+    h = 0
+
+    is_alive =True
+    changes = 0
+
+    current_state = (x,y)
+    history = []
+
+    def __init__(self,x,y,w,h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.current_state = (x,y)
+        self.history.append((x,y,w,h))
+        self.is_alive=True
+
+    def update(self,x,y,w,h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.current_state = (x, y)
+        self.history.append((x, y, w, h))
+
+    def dist(self,x,y):
+        return math.hypot(x - self.x, y - self.y)
+
+    def die(self):
+        self.is_alive = False
+# --------------------------------------
+
 
 
 def main():
 
-    video_path = 'D:\krk4.mp4'
+    video_path = 'D:\krk.mp4'
     cv2.ocl.setUseOpenCL(False)
-
-    tracker = cv2.TrackerBoosting_create()
-
 
     #read video file
     cap = cv2.VideoCapture(video_path)
@@ -15,12 +53,9 @@ def main():
     ret, frame1 = cap.read()
     ret, frame2 = cap.read()
 
-    bbox = (287, 23, 86, 320)
-    ok = tracker.init(frame1, bbox)
-
-
+    counter = 0
     while ret:
-        i = 1
+
         _, frame = cap.read()
 
         d = cv2.absdiff(frame1, frame2)
@@ -38,25 +73,49 @@ def main():
             # get bounding box from countour
             (x, y, w, h) = cv2.boundingRect(c)
 
-            if y < 450:
+            if y < 550:
                 continue
 
-            # draw bounding box
-            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            if not people:
+                people.append(Person(x, y, w, h))
+                continue
 
+            updated = False
+            print(len(people))
+            for person in people:
+                if person.dist(x,y) < 80:
+                    print("person from ",person.x," ",person.y, "updated to ",x," ",y)
+                    person.update(x,y,w,h)
+                    updated = True
+                    person.changes = person.changes +1
+                    break
+            if updated==False:
+                people.append(Person(x, y, w, h))
+            # draw bounding box
+
+        for person in people:
+            if (counter-person.changes > 5):
+                person.die()
+
+        for person in people:
+            if (person.is_alive):
+                cv2.rectangle(frame1, (person.x, person.y), (person.x + person.w, person.y + person.h), (0, 255, 0), 2)
+            else:
+                print ("dead person!")
 
         cv2.imshow("inter", frame1)
 
         if cv2.waitKey(40) == 27:
             break
 
+        counter = counter+1
         frame1 = frame2
         ret, frame2 = cap.read()
-        # j=j+1
+        # j=j+1`
+
     cv2.destroyAllWindows()
     cap.release()
 
 
 main()
 
-#installed with whl files!!!!!
