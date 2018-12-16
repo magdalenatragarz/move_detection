fc069fa920223291300b82fdf06fd43def851f8d
Magda <magdtrag@gmail.com>
2018-12-16 21:44:22 +0100

    commons

--- det_track.py
+++ det_track.py
@@ -3,6 +3,8 @@ import imutils
 import people
 import numpy as np
 import math
+import common
+import colors
 
 VIDEO_PATH = 'D:\krakau.mov'
 PEOPLE_LIST = []
@@ -10,7 +12,6 @@ REAL_PEOPLE = []
 
 def detect():
 
-    cv2.ocl.setUseOpenCL(False)
     cap = cv2.VideoCapture(VIDEO_PATH)
 
     ret, frame1 = cap.read()
@@ -31,32 +32,32 @@ def detect():
         blur = cv2.GaussianBlur(imgray, (5, 5), 1)
         ret, thresh = cv2.threshold(blur, 15, 255, cv2.THRESH_BINARY)
         delete_noises = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, None)
-        kernel = np.ones((6,3), np.uint8)
-        filtered = cv2.dilate(delete_noises, kernel, iterations=3)
+        kernel = np.ones(common.DILATE_VECTOR, np.uint8)
+        filtered = cv2.dilate(delete_noises, kernel, iterations=common.DILATE_ITERATIONS)
 
         _, contours, _ = cv2.findContours(filtered, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for c in contours:
             (x, y, w, h) = cv2.boundingRect(c)
 
-            if cv2.contourArea(c) < 40:
+            if cv2.contourArea(c) < common.MINIMUM_CONTOUR_AREA:
                  continue
 
-            if y < 250:
+            if y < common.SKYLINE:
                 continue
 
             updated = False
             for p in PEOPLE_LIST :
-                if len(p.history) > 5:
+                if len(p.history) > common.CREDIBLE_HISTORY_LENGTH:
                     [coord_x, coord_y] = p.predict_move()
-                    if dist(x,y,coord_x,coord_y) < 5 and not p.updated and p.get_standard_deviation_with_new_point(x,y) < 0.05 and (frame_count - p.frame_count)<20:
+                    if dist(x,y,coord_x,coord_y) < common.CLOSE_NEIGHBOURHOOD and not p.updated and p.get_standard_deviation_with_new_point(x,y) < common.MAX_DEVIATION and (frame_count - p.frame_count)<common.MAX_FRAME_DIFFERENCE:
                         p.update(x,y,frame_count)
                         p.mark_updated()
                         p.how_many_predicted = 0
                         updated = True
                         break
                 else:
-                    if p.dist(x,y) < 15 and p.dist(x,y) > 0 and not p.updated and p.get_standard_deviation_with_new_point(x,y) < 0.05 and (frame_count - p.frame_count)<20 :
+                    if p.dist(x,y) < common.MAX_DISTANCE  and not p.updated and p.get_standard_deviation_with_new_point(x,y) < common.MAX_DEVIATION and (frame_count - p.frame_count)<common.MAX_FRAME_DIFFERENCE :
                         p.update(x,y,frame_count)
                         p.mark_updated()
                         p.how_many_predicted = 0
@@ -66,10 +67,9 @@ def detect():
                 person = people.Person(x,y,frame_count)
                 person.mark_updated()
                 PEOPLE_LIST.append(person)
-                #break
 
         for p in PEOPLE_LIST:
-            if not p.updated and len(p.history) > 10 and p.get_standard_deviation() < 0.05 and p.how_many_predicted < 3 and (frame_count - p.frame_count)<20:
+            if not p.updated and len(p.history) > common.CREDIBLE_HISTORY_LENGTH and p.get_standard_deviation() < common.MAX_DEVIATION and p.how_many_predicted < common.MAX_PREDICTIONS_QUANTITY and (frame_count - p.frame_count)<common.MAX_FRAME_DIFFERENCE:
                 [coord_x, coord_y] = p.predict_move()
                 p.update(coord_x, coord_y, frame_count)
                 p.how_many_predicted += 1
@@ -77,21 +77,18 @@ def detect():
 
         for p in PEOPLE_LIST:
             if p.updated:
-                if len(p.history) > 20 and p.get_standard_deviation() < 0.05:
-                    cv2.rectangle(frame1, (p.x, p.y), (p.x + 10, p.y + 20), (0, 255, 0), 2)
+                if len(p.history) > common.CREDIBLE_HISTORY_LENGTH and p.get_standard_deviation() < common.MAX_DEVIATION:
+                    cv2.rectangle(frame1, (p.x, p.y), (p.x + common.BBOX_WIDTH, p.y + common.BBOX_HEIGHT), colors.blue, common.LINE_WIDTH)
                     index = PEOPLE_LIST.index(p)
-                    cv2.putText(frame1, str(index + 1), (p.x - 10, p.y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (50, 170, 50), 2)
+                    cv2.putText(frame1, str(index + 1), (p.x - common.TEXT_MARIGIN, p.y - common.TEXT_MARIGIN), cv2.FONT_HERSHEY_SIMPLEX, common.TEXT_SIZE, colors.black, common.LINE_WIDTH)
 
-        cv2.imshow("inter", frame1)
-        cv2.imshow("inter2", filtered)
+        cv2.imshow(common.WINDOW_NAME, frame1)
 
-        if cv2.waitKey(40) == 27:
+        if cv2.waitKey(common.DELIM_WAIT) == common.ESC_PRESSED:
             break
 
         frame1 = frame2
         ret, frame2 = cap.read()
-
-
 
     cv2.destroyAllWindows()
     cap.release()
